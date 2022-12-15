611d4c1de2ddf2bc376aa97291a0bcaff89c97f0
yushiran <79855240+yushiran@users.noreply.github.com>
2022-12-15 16:24:22 +0800

    1

---
+++ 俄罗斯方块/基于单片机的俄罗斯方块游戏的实现(改驱动)/game.c
@@ -0,0 +1,11 @@
+#include <reg52.h>
+#include <display_drive.h>
+#include <Tetris_define.h>
+#include <button_drive.h>
+#include <Tetris.h>
+
+void main()
+{	 
+	Init_Max7219();
+  game_start_show();
+}
